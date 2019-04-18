74f82c32544131a98e4b7b9adfa07fdc58ae991b
wiwitonian <wiwit.k@gmail.com>
2019-04-18 17:27:32 +0700

    add Maven Java 11 support

--- src/main/java/com/codewars/kyu8/Main.java
+++ src/main/java/com/codewars/kyu8/Main.java
@@ -5,6 +5,5 @@ public class Main {
         int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
         int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
         System.out.println(Kata.comp(a,b));
-        RandomStringUtils.
     }
 }
